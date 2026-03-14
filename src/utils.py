f3ea0f0d2fac6241a7eadd7eaced447776865da9
bangoim <joaocbbc@outlook.com>
2026-03-14 11:18:00 -0300

    feat: add softmax, layer norm and residual connection utils

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+EPSILON = 1e-6
+
+
+def softmax(x):
+    exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+
+def layer_norm(x, eps=EPSILON):
+    mean = np.mean(x, axis=-1, keepdims=True)
+    var = np.var(x, axis=-1, keepdims=True)
+    return (x - mean) / np.sqrt(var + eps)
+
+
+def residual_add_norm(x, sublayer_out, eps=EPSILON):
+    return layer_norm(x + sublayer_out, eps)
